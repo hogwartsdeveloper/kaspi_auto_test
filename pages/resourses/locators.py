a3d04c08d384888cb3357c5c8ef09d48e178e132
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 22:35:13 +0600

    [FEATURE] add locators for regions

--- pages/resourses/locators.py
+++ pages/resourses/locators.py
@@ -6,3 +6,9 @@ class MainPageLocators:
     PAYMENTS = (By.CLASS_NAME, "payments__items")
     KASPI_RED_MENU = (By.CSS_SELECTOR, ".kaspiRedMenu._clearfix")
     KASPI_GUIDE_BLOCK = (By.CSS_SELECTOR, "div.kaspiGuide-desk-videoTutorBlock")
+
+    TITLE_REGION_CONFIRM = (By.CLASS_NAME, "headerRegionConfirm__title")
+    BUTTON_REGION_CONFIRM = (By.ID, "headerRegionConfirm__ConfirmButton")
+    BUTTON_REGION_NOT_CONFIRM = (By.ID, "headerRegionConfirm__CancelButton")
+
+    REGION = (By.ID, "headerRegionId")
