932a0253228b66f82eba62a219c86ff2e5de549a
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 21:44:31 +0600

    [FEATURE] method for should be kaspi shop

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,11 @@
 from pages.base_page import BasePage
+from pages.resourses.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     def should_be_main_page(self):
-        pass
+        self.should_be_kaspi_shop()
+
+    def should_be_kaspi_shop(self):
+        assert self.is_element_present(*MainPageLocators.SHOP_CATEGORIES), \
+            "Kaspi Shop is nor present"
