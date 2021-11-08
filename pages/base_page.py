562b487f92aa977a4ae8815ef546f041526b5ced
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 23:00:21 +0600

    [FEATURE] method for is not element present

--- pages/base_page.py
+++ pages/base_page.py
@@ -21,3 +21,12 @@ class BasePage:
             return False
 
         return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
