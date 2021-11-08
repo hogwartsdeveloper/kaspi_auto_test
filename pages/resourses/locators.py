b306b059505144f0f584f3dd11aa15c110d474dc
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 21:55:11 +0600

    [FEATURE] add locators payments, kaspi red menu and kaspi guide block

--- pages/resourses/locators.py
+++ pages/resourses/locators.py
@@ -3,3 +3,6 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     SHOP_CATEGORIES = (By.CSS_SELECTOR, "div.shop-categories")
+    PAYMENTS = (By.CLASS_NAME, "payments__items")
+    KASPI_RED_MENU = (By.CSS_SELECTOR, ".kaspiRedMenu._clearfix")
+    KASPI_GUIDE_BLOCK = (By.CSS_SELECTOR, "div.kaspiGuide-desk-videoTutorBlock")
