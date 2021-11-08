5e8a834034ccf54f1f788cbb3dbae2143ec428fa
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 21:40:59 +0600

    [FEATURE] method for is element present web site

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,8 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
@@ -6,3 +11,13 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located((how, what))
+            )
+        except TimeoutException:
+            return False
+
+        return True
