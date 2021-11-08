570bf72816537ddf4c04df30b504f123a4e2e1a1
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 22:25:23 +0600

    [TEST] add test guest should see main page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,6 +1,12 @@
 import pytest
+from pages.main_page import MainPage
+
+
+url = "https://kaspi.kz/"
 
 
 class TestMainPage:
     def test_guest_should_see_main_page(self, browser):
-        pass
+        page = MainPage(browser, url)
+        page.open()
+        page.should_be_main_page()
