b0cafa2241774b70c8243d28a04312ab20405065
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 23:12:26 +0600

    [TEST] add tests main page

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,4 +1,3 @@
-import pytest
 from pages.main_page import MainPage
 
 
@@ -9,4 +8,17 @@ class TestMainPage:
     def test_guest_should_see_main_page(self, browser):
         page = MainPage(browser, url)
         page.open()
+        page.confirm_region()
         page.should_be_main_page()
+
+    def test_guest_see_confirm_region_modal_window(self, browser):
+        page = MainPage(browser, url)
+        page.open()
+        page.should_be_confirm_region_modal_window()
+
+    def test_guest_confirm_region(self, browser):
+        page = MainPage(browser, url)
+        page.open()
+        page.get_confirm_region()
+        page.confirm_region()
+        page.should_be_selected_region()
