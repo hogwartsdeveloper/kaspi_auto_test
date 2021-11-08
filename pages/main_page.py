8d201fdb6123bbe22f8737ad9349fa45adb9b70e
Zhannur <jannuraidynuly@gmail.com>
2021-11-08 22:00:28 +0600

    [FEATURE] create methods for should be main page

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,7 +5,22 @@ from pages.resourses.locators import MainPageLocators
 class MainPage(BasePage):
     def should_be_main_page(self):
         self.should_be_kaspi_shop()
+        self.should_be_kaspi_payments()
+        self.should_be_kaspi_red_menu()
+        self.should_be_kaspi_guide_block()
 
     def should_be_kaspi_shop(self):
         assert self.is_element_present(*MainPageLocators.SHOP_CATEGORIES), \
-            "Kaspi Shop is nor present"
+            "Kaspi Shop is not present"
+
+    def should_be_kaspi_payments(self):
+        assert self.is_element_present(*MainPageLocators.PAYMENTS), \
+            "Kaspi Payments is not present"
+
+    def should_be_kaspi_red_menu(self):
+        assert self.is_element_present(*MainPageLocators.KASPI_RED_MENU), \
+            "Kaspi red menu is not present"
+
+    def should_be_kaspi_guide_block(self):
+        assert self.is_element_present(*MainPageLocators.KASPI_GUIDE_BLOCK), \
+            "Kaspi guide block is not present"
